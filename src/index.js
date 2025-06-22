2d37c500414c4bd180659d9332670ff723090740
I-code1992 <138527013+I-code1992@users.noreply.github.com>
2025-06-22 20:53:40 +0200

    added javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//alert("Hello");
+function generatePoem(event){
+    event.preventDefault();
+
+new Typewriter('#poem', {
+  strings: "hellow there",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+}
+
+
+let poemformElement= document.querySelector("#poem-generator");
+poemformElement.addEventListener("submit", generatePoem);
